1c204ac209dd15c516cb25efe47b66de8f1c1e18
Travis Teeter <Travis_Teeter@dell.com>
2020-03-10 13:07:18 -0700

    This commit will map 4 card objects into HeaderCard components

--- src/components/Headers/HeaderCard.js
+++ src/components/Headers/HeaderCard.js
@@ -2,7 +2,7 @@ import React from "react";
 // reactstrap components
 import { Card, CardBody, CardTitle, Row, Col } from "reactstrap";
 
-const HeaderCard = ({ args }) => {
+const HeaderCard = ( props ) => {
     return (
         <Col lg="6" xl="3">
             <Card className="card-stats mb-4 mb-xl-0">
@@ -13,23 +13,23 @@ const HeaderCard = ({ args }) => {
                     tag="h5"
                     className="text-uppercase text-muted mb-0"
                     >
-                    {args.title}
+                    {props.title}
                     </CardTitle>
                     <span className="h2 font-weight-bold mb-0">
-                    {args.titleValue}
+                    {props.titleValue}
                     </span>
                 </div>
                 <Col className="col-auto">
-                    <div className={`icon icon-shape bg-${args.iconColor} text-white rounded-circle shadow`}>
-                    <i className={`fas fa-${args.icon}`} />
+                    <div className={`icon icon-shape bg-${props.iconColor} text-white rounded-circle shadow`}>
+                    <i className={`fas fa-${props.icon}`} />
                     </div>
                 </Col>
                 </Row>
                 <p className="mt-3 mb-0 text-muted text-sm">
-                <span className={`text-${args.arrowColor} mr-2`}>
-                    <i className={`fas fa-arrow-${args.arrow}`} /> {args.percent}
+                <span className={`text-${props.arrowColor} mr-2`}>
+                    <i className={`fas fa-arrow-${props.arrow}`} /> {props.percent}
                 </span>{" "}
-                <span className="text-nowrap">{args.since}</span>
+                <span className="text-nowrap">{props.since}</span>
                 </p>
             </CardBody>
             </Card>
